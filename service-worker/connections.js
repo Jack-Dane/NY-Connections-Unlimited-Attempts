a386c926812a0e81f5b7333610a06be416ec8f6c
Jack Dane <jackd98.jd@gmail.com>
2024-09-13 08:15:15 +0100

    Set the answers in local storage to be used to check the answers.

--- service-worker/connections.js
+++ service-worker/connections.js
@@ -12,11 +12,12 @@ ${currentDate.getFullYear()}-\
 ${zeroLeadingDate(currentDate.getMonth() + 1)}-\
 ${zeroLeadingDate(currentDate.getDate())}
 `;
-            console.log(dateString);
+            console.log(`Current date string ${dateString}`);
 
             fetch(`https://www.nytimes.com/svc/connections/v2/${dateString}.json`)
                 .then(response => response.json())
-                .then(jsonResponse => console.log(jsonResponse));
+                .then(connectionAnswers => chrome.storage.local.set({connections_result: connectionAnswers}))
+                .catch(error => { console.error(error) });
         }
     }
 );
